63520a7f080cd635be2a3b25d8d291e185de1790
luigui <luisitogas25@gmail.com>
2026-04-23 14:14:26 -0600

    Babys final

--- TiendaServicios.Api.Libro/Modelo/LibreriaMaterial.cs
+++ TiendaServicios.Api.Libro/Modelo/LibreriaMaterial.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TiendaServicios.Api.Libro.Modelo
+namespace TiendaServicios.Api.Libro.Modelo;
 {
     public class LibreriaMaterial
     {

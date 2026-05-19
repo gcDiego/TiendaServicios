3e244ff53d765bcef2f2554c25927221f39c53b1
Diego G <dgcalderon@eglobal.com.mx>
2026-05-19 11:46:01 -0600

    carrito

--- TiendaServicios.Api.CarritoCompra/Program.cs
+++ TiendaServicios.Api.CarritoCompra/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace TiendaServicios.Api.Libro
+namespace TiendaServicios.Api.CarritoCompra
 {
     public class Program
     {
